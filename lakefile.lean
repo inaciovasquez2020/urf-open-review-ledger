aa6a03241c4c4965beb3c96f42a7f688ba3a178a
Inacio F. Vasquez <inaciovasquez2020@gmail.com>
2026-03-21 23:57:45 -0500

    Add root lakefile.lean configuration

--- lakefile.lean
+++ lakefile.lean
@@ -1,7 +1,11 @@
 import Lake
 open Lake DSL
 
-package urf_core
+package URFCore where
+ version := "0.1.0"
+
+lean_lib URFCore
 
 @[default_target]
-lean_lib URFCore
+lean_exe urf_core where
+ root := `URFCore
